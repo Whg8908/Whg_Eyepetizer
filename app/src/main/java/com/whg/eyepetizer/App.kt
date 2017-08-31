171856bc83b8139b6c6f2fc92a11b0e237f2c65e
whg <ghw8908@163.com>
2017-08-31 16:52:43 +0800

    1.添加一个优雅的crash页面

--- app/src/main/java/com/whg/eyepetizer/App.kt
+++ app/src/main/java/com/whg/eyepetizer/App.kt
@@ -3,6 +3,7 @@ package com.whg.eyepetizer
 import android.content.Context
 import android.support.multidex.MultiDex
 import android.support.multidex.MultiDexApplication
+import cat.ereza.customactivityoncrash.config.CaocConfig
 import com.aitangba.swipeback.ActivityLifecycleHelper
 
 /**
@@ -13,6 +14,8 @@ class App : MultiDexApplication(){
     override fun onCreate() {
         super.onCreate()
         registerActivityLifecycleCallbacks(ActivityLifecycleHelper.build())
+        CaocConfig.Builder.create().apply()
+
     }
 
     /**
